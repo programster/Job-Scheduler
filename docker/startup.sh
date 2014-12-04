47b933d56f466b7136845ab7fe1261a6be70e0c6
Stuart Page <sdpagent@gmail.com>
2014-12-04 15:52:52 +0000

    dockerfile now installs vim instead of vim.tiny installing and using the cron service for keeping the docker container open. This allows the developer to kill the java process and restart it for debugging etc.

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,5 +1,8 @@
 # Please do not manually call this file!
 # This script is run by the docker container when it is "run"
 
-/usr/bin/java -jar /root/scheduler/scheduler.jar
+/usr/bin/java -jar /root/scheduler/scheduler.jar &
+
+# Start the cron service in the foreground
+cron -f
 
