d05e15f6d04092a0c0d197015371c8d6458b62b2
Stuart Page <sdpagent@gmail.com>
2014-12-04 15:54:16 +0000

    The start container script now calls the image by name rather than relying on its position in the docker images

--- docker/start-container.sh
+++ docker/start-container.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-sudo docker run -d -p 3901:3901 `docker images -q | sed -n 2p`
+#!/bin/bash
+
+# ensure running bash
+if ! [ -n "$BASH_VERSION" ];then
+    echo "this is not bash, calling self with bash....";
+    SCRIPT=$(readlink -f "$0")
+    /bin/bash $SCRIPT
+    exit;
+fi
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT") 
+cd $SCRIPTPATH
+
+# load the variables
+#source ../../settings/docker_settings.sh
+PROJECT_NAME="scheduler"
+
+CONTAINER_IMAGE="$PROJECT_NAME"
+
+docker kill $PROJECT_NAME
+docker rm $PROJECT_NAME
+
+docker run -d \
+-p 3901:3901 \
+--name="$PROJECT_NAME" \
+$CONTAINER_IMAGE
