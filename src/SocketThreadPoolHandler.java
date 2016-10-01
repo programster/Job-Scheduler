87bc8f0fcb16eaf114a6aef09124153bb091e681
Programster <sdpagent@gmail.com>
2016-10-01 16:18:46 +0000

    Updated the SocketThreadPoolHandler so that it handles multiple of the new SocketConnection objects for persistent client connections.

--- src/SocketThreadPoolHandler.java
+++ src/SocketThreadPoolHandler.java
@@ -1,8 +1,14 @@
 
 import java.net.Socket;
+import java.util.Iterator;
+import java.util.concurrent.BlockingQueue;
 
 /*
- * This is an thread that belongs in a "pool" that handles socket connection.
+ * This is a "worker thread" that handles socket connections. There will be a pool of these workers
+ * which work in parallel to handle all client connections. The number of connections will be
+ * evenly distributed across these workers, with each worker being able to handle multiple 
+ * connections.
+ *
  * Using this architecture prevents the possibility that we overload our server with threads and 
  * also removes the overhead of spawning threads constantly.
  */
@@ -10,26 +16,66 @@ import java.net.Socket;
 
 public class SocketThreadPoolHandler extends Thread
 {
+    private BlockingQueue<SocketConnection> m_clientSockets;
     private static SocketListener s_listener = null;
+    
     
     public SocketThreadPoolHandler(SocketListener listener)
     {
         s_listener = listener;
     }
     
+    
+    /**
+     * Add a client socket connection to this handlers collection that it is responsible for.
+     * @param clientSocket 
+     */
+    public void addSocket(Socket clientSocket)
+    {
+        SocketConnection conn = new SocketConnection(clientSocket);
+        m_clientSockets.add(conn);
+    }
+    
+    
+    /**
+     * Return the number of sockets this thread is currently handling. Useful for the manager
+     * to decide which worker it should give new connections to for even distribution.
+     * @return the number of sockets
+     */
+    public int getSocketCount()
+    {
+        return m_clientSockets.size();
+    }
+    
+    
+    
+    /**
+     * Loop over our collection of connections and pass off any requests to the 
+     * HandlerLogic for handling communication protocol
+     */
     public void run()
     {
         while (true)
         {
-            try
+            Iterator socketIterator = m_clientSockets.iterator();
+            
+            while (socketIterator.hasNext())
             {
-                // This will automatically wait/block when nothing is available.
-                Socket socket = s_listener.getWaitingSocket();
-                HandlerLogic.handleSocket(socket);
-            }
-            catch (Exception e)
-            {
-                System.out.println("SocketThreadPoolHandler: " + e.getMessage().toString());
+                try
+                {
+                    // This will automatically wait/block when nothing is available.
+                    SocketConnection connection = (SocketConnection)socketIterator.next();
+                    HandlerLogic.handleSocket(connection);
+                    
+                    if (connection.isClosed())
+                    {
+                        socketIterator.remove();
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.out.println("SocketThreadPoolHandler: " + e.getMessage().toString());
+                }
             }
         }
     }
