cedc743aba7ad103d33339ac5ae06c10ae3fcbab
Programster <sdpagent@gmail.com>
2016-10-01 17:55:29 +0000

    updatng the test_task_timeout test so that it gets skipped if the max_lock_time is infinite.

--- php_sdk/testing/test_cases/test_task_timeout.class.php
+++ php_sdk/testing/test_cases/test_task_timeout.class.php
@@ -22,7 +22,7 @@ class TestTaskTimeout extends TestAbstract
         
         return $message;
     }
-
+    
     
     /**
      * The main logic of the test.
@@ -31,45 +31,59 @@ class TestTaskTimeout extends TestAbstract
     public function test() 
     {
         global $globals;
-        
-        $scheduler = $this->getScheduler();
-        
-        $not_important_id = $scheduler->addTask($name="not_important", 
-                                                $context=array(), 
-                                                $dependencies=array(),
-                                                $priority=1);
-        
-        $high_importance_id = $scheduler->addTask($name="high_importance", 
-                                                  $context=array(), 
-                                                  $dependencies=array(),
-                                                  $priority=9); 
-        
-        # Now fetch the tasks and hope that they come in the correct order.
         $this->m_successful = true; # default to true and let the funcs below set to false if fail
         
-        $this->fetch_expected_task($high_importance_id, 
-                                   "high_importance", 
-                                   $this->m_recieved_tasks);
-        
-        $sleepTime = $globals['MAX_LOCK_TIME'] + 2; // Has to be longer than the timeout time
-        
-        print "Sleeping for task timout period of: " . $sleepTime . ' seconds.' . 
-              PHP_EOL;
-        
-        sleep($sleepTime);
-        
-        
-        # Even though the important task should have exceeded its MAX_LOCK_TIME we should still 
-        # only get the not_important task first as all available tasks should be completed before
-        # looking for timeout tasks. This is is because we want to give them as much time as
-        # possible and not waste CPU looping through tasks that are in progress.
-        
-        $this->fetch_and_complete_expected_task($not_important_id, 
-                                                "not_important", 
-                                                $this->m_recieved_tasks);
-        
-        $this->fetch_and_complete_expected_task($high_importance_id, 
-                                                "high_importance", 
-                                                $this->m_recieved_tasks);
+        if ($globals['MAX_LOCK_TIME'] == 0)
+        {
+            print "Skipping TestTaskTimeout is pointless because MAX_LOCK_TIME is set to infinite" . PHP_EOL;
+        }
+        else
+        {
+            $scheduler = $this->getScheduler();
+            
+            $not_important_id = $scheduler->addTask(
+                $name="not_important", 
+                $context=array(), 
+                $dependencies=array(),
+                $priority=1
+            );
+            
+            $high_importance_id = $scheduler->addTask(
+                $name="high_importance", 
+                $context=array(), 
+                $dependencies=array(),
+                $priority=9
+            ); 
+            
+            # Now fetch the tasks and hope that they come in the correct order.
+            $this->fetch_expected_task(
+                $high_importance_id, 
+                "high_importance", 
+                $this->m_recieved_tasks
+            );
+            
+            $sleepTime = $globals['MAX_LOCK_TIME'] + 2; // Has to be longer than the timeout time
+            
+            print "Sleeping for task timout period of: " . $sleepTime . ' seconds.' . 
+                  PHP_EOL;
+            
+            sleep($sleepTime);
+            
+            # Even though the important task should have exceeded its MAX_LOCK_TIME we should still 
+            # only get the not_important task first as all available tasks should be completed before
+            # looking for timeout tasks. This is is because we want to give them as much time as
+            # possible and not waste CPU looping through tasks that are in progress.
+            $this->fetch_and_complete_expected_task(
+                $not_important_id, 
+                "not_important", 
+                $this->m_recieved_tasks
+            );
+            
+            $this->fetch_and_complete_expected_task(
+                $high_importance_id, 
+                "high_importance", 
+                $this->m_recieved_tasks
+            );
+        }
     }
 }
