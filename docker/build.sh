95c8654413fcbcea55eda43dcac77766176940ae
Programster <sdpagent@gmail.com>
2016-10-01 10:49:10 +0000

    updating the build script to push to my registry. Other users can change the registry variable to push to their own.

--- docker/build.sh
+++ docker/build.sh
@@ -8,6 +8,14 @@ if ! [ -n "$BASH_VERSION" ];then
     exit;
 fi
 
+##############
+## Settings ##
+##############
+# Change REGISTRY to "" if you dont have a registry.
+REGISTRY="programster"
+PROJECT_NAME="job-scheduler"
+
+
 # Get the path to script just in case executed from elsewhere.
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
@@ -16,20 +24,23 @@ cd $SCRIPTPATH
 cp -f Dockerfile ../.
 cd ../.
 
-PROJECT_NAME="scheduler"
-
 # Ask the user if they want to use the docker cache
 read -p "Do you want to use a cached build (y/n)? " -n 1 -r
 echo ""   # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    docker build --tag "$PROJECT_NAME" .
+    docker build --tag "$REGISTRY/$PROJECT_NAME" .
 else
-    docker build --tag "$PROJECT_NAME" --no-cache .
+    docker build --tag "$REGISTRY/$PROJECT_NAME" --no-cache .
 fi
 
 # Clean up - Remove the dockerfile we moved up.
 rm $SCRIPTPATH/../Dockerfile
+
+# Push to our registry if registry is set
+if [ -n "$REGISTRY" ]; then
+    docker push $REGISTRY/$PROJECT_NAME
+fi
 
 echo "Run the container with the following command:"
 echo "bash run-container.sh"
